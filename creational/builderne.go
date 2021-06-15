aa24bf6e09165f04506d672319647d9f4f21ceb3
HiepPP <zendragon93@gmail.com>
2021-06-16 05:44:11 +0700

    practice builder pattern

---
+++ creational/builderne.go
@@ -0,0 +1,58 @@
+package creational
+
+type user struct {
+	name  string
+	phone string
+	role  string
+}
+
+// Nếu user implement interface này, mọi property sẽ được che giấu, không thể gọi trực tiếp mà phải thông qua function get
+type UserInformation interface {
+	getUserName() string
+	getUserPhone() string
+	getUserRole() string
+}
+
+type UserBuilder interface {
+	CreateName(name string) UserBuilder
+	CreatePhone(phone string) UserBuilder
+	CreateRole(role string) UserBuilder
+	BuildUser() UserInformation // không return về trực tiếp struct user mà thông qua interface để che giấu property
+}
+
+func (u *user) getUserName() string {
+	return u.name
+}
+func (u *user) getUserPhone() string {
+	return u.phone
+}
+func (u *user) getUserRole() string {
+	return u.role
+}
+
+func (u *user) CreateName(name string) UserBuilder { //return về interface UserBuilder có thể tiếp tục call func thuộc về interface này
+	u.name = name
+	return u
+}
+
+func (u *user) CreatePhone(phone string) UserBuilder {
+	u.phone = phone
+	return u
+}
+
+func (u *user) CreateRole(role string) UserBuilder {
+	u.role = role
+	return u
+}
+
+func (u *user) BuildUser() UserInformation {
+	return u
+}
+
+func createUser() *user {
+	return new(user)
+}
+
+//func newUser(name string, phone string) *user {
+//	return &user{name: name, phone: phone}
+//}
---
+++ creational/builderne_test.go
@@ -0,0 +1,16 @@
+package creational
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestBuilderNe(t *testing.T) {
+	admin := createUser().CreateName("Administator").CreatePhone("0123213").CreateRole("Admin").BuildUser()
+	if admin.getUserRole() == "" {
+		t.Error("wrong")
+	}
+	name := admin.getUserName()
+	fmt.Println(name)
+	fmt.Printf("%v", admin)
+}
